ec176dc9e18909cc5644b26ac4a76adf2f7afd16
helenaskagerlid <helena.skagerlid@medieinstitutet.se>
2023-12-02 11:05:46 +0100

    :construction: move products array

---
+++ productsarray.mjs
@@ -0,0 +1,51 @@
+
+const fantasyBabyMonsters = [
+    {
+        name: 'Ulla brown hairy turtle',
+        productNo: 'Product-1',
+        price: 888,
+        category: 'Hairy',
+        rating: 5,
+        amount: 0,
+        image: {
+            src: 'assets/fantasy-monster-plushie-brown-hairy-turtle.jpg',
+            alt: 'a fantasy plushie that looks like a brown little baby turtle with brown long fur',
+            width: '500px',
+            height: 'auto',
+            loading: 'lazy'
+        }
+    },
+    {
+        name: 'Britta Baby snail',
+        productNo: 'Product-2',
+        price: 555,
+        category: 'Slimy',
+        rating: 4,
+        amount: 0,
+        image: {
+            src: 'assets/fantasy-monster-plushie-happy-snail.jpg',
+            alt: 'a fantasy monster plushie that looks like a light blue snail that is cute and happy',
+            width: '500px',
+            height: 'auto',
+            loading: 'lazy'
+        }
+    },
+    {
+        name: 'Amy Baby spider',
+        productNo: 'Product-3',
+        price: 777,
+        category: 'Hairy',
+        rating: 3,
+        amount: 0,
+        image: {
+            src: 'assets/fantasy-monster-plushie-baby-spider.jpg',
+            alt: 'a fantasy monster plushie that looks like a hairy baby spider',
+            width: '500px',
+            height: 'auto',
+            loading: 'lazy'
+        }
+        
+    },
+  ]
+
+  export default fantasyBabyMonsters;
