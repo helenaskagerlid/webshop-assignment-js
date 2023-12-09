582936b39a96d71c5b203b826dc5bb3d637b97e6
helenaskagerlid <helena.skagerlid@medieinstitutet.se>
2023-12-09 22:29:57 +0100

    :construction: add more products

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/lVSydX1g)
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=12877967&assignment_repo_type=AssignmentRepo)
+
+# Monster Webshop
+
+## About 
+
+This webshop was a individual project we did in our Introduction Course in Javascript (JS). The assigment was to learn the basics in JS by creating a webshop from an imagined client. The client had specific requierments that we were supposed to able to deliver to them. Over the course of 4 weeks I created this webshop.
+
+## Link to the project
+
+https://medieinstitutet.github.io/fed23d-js-grundkurs-webshop-helenaskagerlid/
+
+## Learning Focus
+
+The areas we got examinated on:
+
+- Logic & Program Flow
+- Comments and Self-Documenting Code
+- High Code Quality, Conventions
+- Conditionals (if statements)
+- Events
+- DOM Manipulation
+- Functions
+- Variables
+- Arithmetic
+- Objects
+- Arrays
+- Timers
+- Loops
+- Dates
+
+## The team
+
+Helena Skagerlid - https://github.com/helenaskagerlid
+
+## Tech Stack
+
+- HTML
+- Sass
+- Javascript
+- Vite
--- productsarray.mjs
+++ productsarray.mjs
@@ -1,9 +1,9 @@
 
 const fantasyBabyMonsters = [
     {
-        name: 'Ulla brown hairy turtle',
+        name: 'Ulla turtle',
         productNo: 'Product-1',
-        price: 888,
+        price: 444,
         category: 'Hairy',
         rating: 5,
         amount: 0,
@@ -16,9 +16,9 @@ const fantasyBabyMonsters = [
         }
     },
     {
-        name: 'Britta Baby snail',
+        name: 'Britta snail',
         productNo: 'Product-2',
-        price: 555,
+        price: 222,
         category: 'Slimy',
         rating: 4,
         amount: 0,
@@ -31,15 +31,143 @@ const fantasyBabyMonsters = [
         }
     },
     {
-        name: 'Amy Baby spider',
+        name: 'Amy spider',
         productNo: 'Product-3',
-        price: 777,
+        price: 444,
         category: 'Hairy',
         rating: 3,
         amount: 0,
         image: {
             src: 'assets/fantasy-monster-plushie-baby-spider.jpg',
             alt: 'a fantasy monster plushie that looks like a hairy baby spider',
+            width: '500',
+            height: 'auto',
+            loading: 'lazy'
+        }
+        
+    },
+    {
+        name: 'Sigrid Caterpillar',
+        productNo: 'Product-4',
+        price: 333,
+        category: 'Slimy',
+        rating: 4,
+        amount: 0,
+        image: {
+            src: 'assets/fantasy-monster-plushie-baby-caterpillar.jpg',
+            alt: 'a fantasy monster plushie that looks like a blue caterpillar',
+            width: '500',
+            height: 'auto',
+            loading: 'lazy'
+        }
+        
+    },
+    {
+        name: 'Svensson Pickles',
+        productNo: 'Product-5',
+        price: 333,
+        category: 'Extra odd',
+        rating: 4.4,
+        amount: 0,
+        image: {
+            src: 'assets/fantasy-monster-plushie-baby-pickles.jpg',
+            alt: 'two fantasy monster plushies that looks like sleeping pickles',
+            width: '500',
+            height: 'auto',
+            loading: 'lazy'
+        }
+        
+    },
+    {
+        name: 'Snail Family',
+        productNo: 'Product-6',
+        price: 444,
+        category: 'Slimy',
+        rating: 5,
+        amount: 0,
+        image: {
+            src: 'assets/fantasy-monster-plushie-baby-snail-family.jpg',
+            alt: 'five fantasy monsters plushies that looks like a family of cute snails',
+            width: '500',
+            height: 'auto',
+            loading: 'lazy'
+        }
+        
+    },
+    {
+        name: 'Isa Caterpillar',
+        productNo: 'Product-7',
+        price: 222,
+        category: 'Slimy',
+        rating: 4.2,
+        amount: 0,
+        image: {
+            src: 'assets/fantasy-monster-plushie-black-caterpillar.jpg',
+            alt: 'a fantasy monster plushie that looks like a black caterpillar bat',
+            width: '500',
+            height: 'auto',
+            loading: 'lazy'
+        }
+        
+    },
+    {
+        name: 'Kerstin the Troll',
+        productNo: 'Product-8',
+        price: 333,
+        category: 'Extra odd',
+        rating: 4.7,
+        amount: 0,
+        image: {
+            src: 'assets/fantasy-monster-plushie-chilling-troll.jpg',
+            alt: 'a fantasy monster plushie that looks like a chilling troll',
+            width: '500',
+            height: 'auto',
+            loading: 'lazy'
+        }
+        
+    },
+    {
+        name: 'Agnes Turtle',
+        productNo: 'Product-9',
+        price: 333,
+        category: 'Hairy',
+        rating: 4.3,
+        amount: 0,
+        image: {
+            src: 'assets/fantasy-monster-plushie-hairy-turtle-with-guitar.jpg',
+            alt: 'a fantasy monster plushie that looks like a hairy turtle with a guitar',
+            width: '500',
+            height: 'auto',
+            loading: 'lazy'
+        }
+        
+    },
+    {
+        name: 'Troll Family',
+        productNo: 'Product-10',
+        price: 555,
+        category: 'Extra odd',
+        rating: 4.6,
+        amount: 0,
+        image: {
+            src: 'assets/fantasy-monster-plushie-troll-family.jpg',
+            alt: 'two fantasy monster plushies that looks like two hairy trolls, one small and one big',
+            width: '500',
+            height: 'auto',
+            loading: 'lazy'
+        }
+        
+    },
+    {
+        name: 'Getrud the Bat',
+        productNo: 'Product-11',
+        price: 333,
+        category: 'Hairy',
+        rating: 4.2,
+        amount: 0,
+        image: {
+            src: 'assets/fantasy-monster-plushie-white-bat.jpg',
+            alt: 'a fantasy monster plushie that looks like a hairy albino bat',
             width: '500',
             height: 'auto',
             loading: 'lazy'
